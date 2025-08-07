475a37662db4bbeed695334a10a141c4f8b19af0
ab <ab@101lab.pl>
2025-08-07 09:49:34 +0200

    Make `CalcButton` cursor-pointer conditional based on disabled state

--- src/components/CalcButton.tsx
+++ src/components/CalcButton.tsx
@@ -8,7 +8,7 @@ interface CalcButtonProps {
 export default function CalcButton({ children, disabled }: CalcButtonProps) {
   return (
     <button
-      className="text-2xl w-[60px] h-[60px] cursor-pointer"
+      className={`text-2xl w-[60px] h-[60px] ${!disabled && "cursor-pointer"}`}
       disabled={disabled}
     >
       {children}
